b00f163a58179deea5d6aafcfb45e0cc5a507e58
Manuel Santiago Hernández Gutiérrez <A01338717@itesm.mx>
2017-08-24 12:35:52 -0500

    cambio en variables2

--- Variables2.java
+++ Variables2.java
@@ -1,7 +1,19 @@
 public class Variables2 {
 
 	public static void main (String[] args) {
-	
-	
+
+		char inicialNombre, inicialApellido;
+
+		inicialNombre = 'S';
+		inicialApellido = 'H';
+
+		codigoControl = '\n';
+
+		System.out.println("Hola " + inicialNombre);
+		System.out.println("Código:  " + codigoControl);
+		System.out.println("\"Hola de nuevo \r" + inicialApellido);
+
+
+
 	}
 }
