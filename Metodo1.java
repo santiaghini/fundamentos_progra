d3b625c9ea5a656a74b93fe726913e998bb42d45
Manuel Santiago Hernández Gutiérrez <A01338717@itesm.mx>
2017-08-28 12:47:15 -0500

    Clase 28 ago

---
+++ Metodo1.java
@@ -0,0 +1,35 @@
+/*
+Nuevo ejeplo del uso
+de métodos
+Por Santiago Hernández
+*/
+
+import java.util.Scanner;	//IMPORTACON DE LA BIBLIOTECA PARA LEER DE TECLADO
+
+public class Metodo1 {
+
+	public static void main(String[] args) {
+
+		String nombre = "Santi";
+
+		Scanner lector = new Scanner(System.in);
+
+		System.out.printf("Dame tu nombre ahora%n");
+		nombre = lector.nextLine();
+
+		System.out.printf("Dame tu edad ahora%n");
+		int edad = lector.nextInt();
+
+		imprime(nombre , edad);
+
+		System.out.printf("Between llamadas al método.%n");
+
+	}
+
+	public static void imprime(String nom , int edad) {
+		int anio = 2017 - edad;
+		System.out.printf("Comostas %s , naciste en %d - el papá. %n" , nom , anio);
+
+	}
+
+}
